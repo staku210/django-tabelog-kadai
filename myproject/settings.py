35dc89c916bbe7d04edad4d1ca67ffaa0364f2e6
staku0210 <banbentameng2@gmail.com>
2025-12-12 17:35:41 +0900

    settings.pyを修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -167,8 +167,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #画像関連の設定
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media_local'
+#MEDIA_URL='/media/'
+#MEDIA_ROOT=BASE_DIR/'media_local'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
