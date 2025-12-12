c839ae0115f54f1c91b8539301803f810ae9f00d
staku0210 <banbentameng2@gmail.com>
2025-12-12 17:31:22 +0900

    settings.pyの修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
