36da8f74145799731e8500cb8856fe071a71346e
staku0210 <banbentameng2@gmail.com>
2025-12-12 18:28:58 +0900

    settings.pyを編集

--- myproject/settings.py
+++ myproject/settings.py
@@ -163,7 +163,7 @@ MEDIA_URL='/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_NAME'),
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': env('CLOUDINARY_API_KEY'),
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
