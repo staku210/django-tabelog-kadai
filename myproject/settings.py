43f1371af36f2449da85110c8ebfbb27b33c6492
staku0210 <banbentameng2@gmail.com>
2025-12-14 12:01:24 +0900

    Fix static files config

--- myproject/settings.py
+++ myproject/settings.py
@@ -151,6 +151,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
