80030edb7e7465ee0adc4d084513fcb8c7761dda
staku0210 <banbentameng2@gmail.com>
2025-12-14 16:53:28 +0900

    Fix static file settings for Heroku + WhiteNoise

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
